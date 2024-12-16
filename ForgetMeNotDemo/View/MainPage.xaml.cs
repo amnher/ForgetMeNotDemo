457c0e0dd4c9a910bcb017b4732a00dd35f2d1cc
Amnher <amnher@gmail.com>
2024-12-16 17:46:56 +0800

    Initial commits on this new project

--- ForgetMeNotDemo/View/MainPage.xaml.cs
+++ ForgetMeNotDemo/View/MainPage.xaml.cs
@@ -1,15 +1,23 @@
-﻿namespace ForgetMeNotDemo.View
+﻿using ForgetMeNotDemo.ViewModel;
+
+namespace ForgetMeNotDemo.View
 {
     public partial class MainPage : ContentPage
     {
-        
+        private MainViewModel vm = new MainViewModel();
 
         public MainPage()
         {
             InitializeComponent();
+            BindingContext = vm;
+        }
+        protected override void OnAppearing()
+        {
+            base.OnAppearing();
+            vm.FullName = "Jesse Liberty";
         }
 
-        
+
     }
 
 }
--- ForgetMeNotDemo/ViewModel/MainViewModel.cs
+++ ForgetMeNotDemo/ViewModel/MainViewModel.cs
@@ -11,4 +11,6 @@ public partial class MainViewModel
 {
     [ObservableProperty]
     private string fullName;
+
+    
 }
